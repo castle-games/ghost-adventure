1500cf2fcd0ad78c018ceed5202a699a76fc2090
schazers <schazers@gmail.com>
2018-07-24 21:40:42 -0700

    gui stubs: file menu, windows, shapes, color, etc.

--- main.lua
+++ main.lua
@@ -2,8 +2,18 @@ local desc = "You are in a room. Press X to leave and go outside."
 
 local room = "turret"
 
+local square = {
+  x = 300,
+  y = 200,
+  width = 50.0,
+  r = 1.0,
+  g = 0.4,
+  b = 0.4,
+  a = 1.0
+}
+
 function love.draw()
-  love.graphics.print(desc, 400, 300)
+  love.graphics.rectangle("fill", square.x, square.y, square.width, square.width)
 end
 
 function love.update(dt)
@@ -12,7 +22,41 @@ function love.update(dt)
 
   end
 
-  if room == "turret"
+  --Todo: play/edit
+
+  tui.beginMainMenuBar()
+  if tui.beginMenu("File", true) then
+    if (tui.menuItem("New", "Cmd + n", false, true)) then
+      -- Todo:
+      print("new file")
+    end
+    if (tui.menuItem("Open", "Cmd + o", false, true)) then
+      -- Todo:
+      print("open file")
+    end
+    if (tui.menuItem("Save", "Cmd + s", false, true)) then
+      -- Todo:
+      print("save file")
+    end
+    tui.endMenu()
+  end
+  tui.endMainMenuBar()
+
+  tui.beginWindow('Shapes!')
+  tui.text('Square')
+  square.r, square.g, square.b, square.a = tui.colorEdit4("color", square.r, square.g, square.b, square.a)
+  tui.endWindow()
+
+  tui.beginWindow('Rooms')
+  --Todo:
+  tui.endWindow()
+
+  --if room == "turret"
+
+  tui.beginWindow('Game')
+  tui.text(desc)
+  tui.endWindow()
+
   if love.keyboard.isDown("x") then
     desc = "You are outside now. The game is now over."
   end
